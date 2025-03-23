00abb07168a89492d4c013e60c5288993bc9bd75
remplerus <mail@rempler.com>
2025-03-23 16:55:32 +0100

    initial release for 1.20

---
+++ common/src/main/java/com/rempler/jawgm/CommonClass.java
@@ -0,0 +1,27 @@
+package com.rempler.jawgm;
+
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class CommonClass {
+    public static void newFunction(NonNullList<ItemStack> items, int count, Item item) {
+        if (count == 1) {
+            for (int i = 0; i < items.size(); i++) {
+                if (items.get(i).is(item)) {
+                    items.set(i, ItemStack.EMPTY);
+                    break;
+                }
+            }
+        } else if (count > 1) {
+            for (int i = 0; i < items.size(); i++) {
+                if (items.get(i).is(item)) {
+                    ItemStack itemStack = item.getDefaultInstance();
+                    itemStack.setCount(count -1);
+                    items.set(i, itemStack.copy());
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ fabric/src/main/java/com/rempler/jawgm/JAWGM.java
@@ -0,0 +1,10 @@
+package com.rempler.jawgm;
+
+import net.fabricmc.api.ModInitializer;
+
+public class JAWGM implements ModInitializer {
+    
+    @Override
+    public void onInitialize() {
+    }
+}
